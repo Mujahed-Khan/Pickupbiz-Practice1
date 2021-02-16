5efc04f46506bee6e3a9f0ba778bbe7a91cd199a
Mujahed-Khan <pmujahedk@gmail.com>
2021-02-16 16:00:20 +0530

    redux

---
+++ src/State/Reducers/GroupReducer.js
@@ -0,0 +1,23 @@
+const GroupReducer = (state=GroupsDefaultState) => {
+    
+}
+    
+const GroupsDefaultState = {
+    groups: [
+        {
+            name: "Redux Practical",
+            image: "Assets/Moonlight.jpg",
+            desc:"This is First Group With Redux"
+        },
+        {
+            name: "Redux Practical",
+            image: "Assets/Moonlight.jpg",
+            desc:"This is First Group"
+        },
+        {
+            name: "Redux Practical",
+            image: "Assets/Moonlight.jpg",
+            desc:"This is First Group"
+        },
+    ]
+}
